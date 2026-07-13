6b5646db5f555ecabcd84b869b01ff9535d046e5
hardyz0517 <hardyz.0517@gmail.com>
2026-07-14 06:01:22 +0800

    feat: connect application theme runtime

--- src/main.tsx
+++ src/main.tsx
@@ -7,19 +7,25 @@ import { installDesktopWebViewGuards } from "@/lib/desktopGuards";
 import { UpdaterProvider } from "@/features/updater/UpdaterProvider";
 import { QueryErrorNotifier } from "@/lib/query/QueryErrorNotifier";
 import { queryClient } from "@/lib/query/queryClient";
+import { ThemeProvider } from "@/theme/ThemeProvider";
+import { initializeTheme } from "@/theme/themeBootstrap";
 import "@/styles.css";
+
+const initialTheme = initializeTheme();
 
 installDesktopWebViewGuards();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <QueryErrorNotifier />
-        <UpdaterProvider>
-          <App />
-        </UpdaterProvider>
-      </ToastProvider>
-    </QueryClientProvider>
+    <ThemeProvider initialSnapshot={initialTheme}>
+      <QueryClientProvider client={queryClient}>
+        <ToastProvider>
+          <QueryErrorNotifier />
+          <UpdaterProvider>
+            <App />
+          </UpdaterProvider>
+        </ToastProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
